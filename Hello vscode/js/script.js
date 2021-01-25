1ab70d980a4456d8db29656a160df9f5cb1c9da6
Ilya <churakov018@gmail.com>
2021-01-25 20:38:12 +0300

    My first console application

--- Hello vscode/js/script.js
+++ Hello vscode/js/script.js
@@ -1,17 +1,17 @@
-let number = 5;
-const leftDorderWidth = 1;
+// let number = 5;
+// const leftDorderWidth = 1;
 
-number = 10;
-console.log(number);
+// number = 10;
+// console.log(number);
 
-const obj = {
-    name: "John",
-    age: 25,
-    isMarried: "Не женат"
-};
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: "Не женат"
+// };
 
-let arr = ['one', 'two', 'three', 'four', 5, {obj}];
-console.log(arr[5]);
+// let arr = ['one', 'two', 'three', 'four', 5, {obj}];
+// console.log(arr[5]);
 
 //const vozrast = prompt("Укажите ваш возраст", "18");
 //console.log(vozrast + 5);
@@ -31,5 +31,25 @@ console.log(arr[5]);
 // // const category = '32';
 // console.log(`https//Izhevsk/S.Votkinskoe/${category}/53`);
 
-const Privet = prompt('Введите ваше имя', '');
-alert(`Привет, ${Privet}`);
+// const Privet = prompt('Введите ваше имя', '');
+// alert(`Привет, ${Privet}`);
+
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+      personalMovieDB.movies[a] = b;
+      personalMovieDB.movies[c] = d;
+
+      console.log(personalMovieDB);
