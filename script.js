f53235970cca9e838f5ee29d6649127b3450e50e
Ilya <churakov018@gmail.com>
2021-01-26 18:01:58 +0300

    Исправил точки с запятой

---
+++ script.js
@@ -0,0 +1,20 @@
+const num = 50;
+// switch (num) {
+//     case 49:
+//         console.log('Wrust');
+//         break;
+//     case 50:
+//         console.log('Successfull');
+//         break;
+//     default:
+//         console.log("Let's try again");
+//         break;
+// }
+
+if (num > 50) {
+    console.log('Много');
+} else if (num < 50) {
+    console.log('Мало');
+} else {
+    console.log('В точку');
+}
